20c32952fc146c6f06216b14e6167c7e483b2163
Jason Paul <jp555soul@gmail.com>
2023-11-09 20:44:56 -0800

    feat: moved to browser notifications

--- app.js
+++ app.js
@@ -3,15 +3,32 @@ require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const socketIo = require('socket.io');
 
 const token = process.env.BOT_TOKEN;
 const app = express();
 const port = process.env.PORT || 3000;
 const bot = new TelegramBot(token);
+const server = require('http').Server(app);
+const io = socketIo(server);
 
 bot.setWebHook(process.env.APP_URL + bot.token);
 
+app.use(express.static('public'));  
 app.use(bodyParser.json());
+
+bot.on('pinned_message', (msg) => {
+    const chatId = msg.chat.id;
+    const messageId = msg.pinned_message.message_id;
+    const messageText = msg.pinned_message.text;
+    bot.sendMessage(chatId, 'Wiz pinned a message!');
+    io.emit('pinned_message', { messageId, messageText });
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
 
 app.get('/', (req, res) => {
     res.status(200).send("Wiz! Wiz! That's the way");
@@ -26,7 +43,3 @@ app.listen(port, () => {
     console.log(`Express server is listening on ${port}`);
 });
 
-bot.on('pinned_message', (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'A message was pinned!');
-});
